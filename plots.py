96857c7ed2824f40ae3fde6a3b531077e57dff3d
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-23 15:26:31 -0500

    plotea los resultados del bono

---
+++ plots.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+datos = np.genfromtxt('datos.csv', delimiter=',')
+
+time = datos[:,0]
+u = datos[:,1]
+uInicial = datos[:,2]
+
+
+plt.plot(time,uInicial, label = 'U inicial', c='k')
+plt.plot(time,u, label = 'U final', c='c')
+plt.title('Conveccion lineal')
+plt.legend(loc=0)
+plt.show()
+
